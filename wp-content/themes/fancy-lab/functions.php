da4266d698dc5b51ef3bd064d88b157f2d2f0adf
vinhpt99 <vinhpt72@gmail.com>
2025-03-27 22:34:52 +0700

    Enqueuing stylesheets the right way #2

--- wp-content/themes/fancy-lab/functions.php
+++ wp-content/themes/fancy-lab/functions.php
@@ -7,6 +7,6 @@ add_action('wp_enqueue_scripts', 'fancylab_enqueue_styles');
 
 function fancy_lab_scripts()
 {
-  wp_enqueue_style('fancy-lab-style', get_stylesheet_uri(), array(), '1.0', 'all');
+  wp_enqueue_style('fancy-lab-style', get_stylesheet_uri(), array(), fileatime(get_template_directory()) . 'style.css', 'all');
 }
 add_action('wp_enqueue_scripts', 'fancy_lab_scripts');
--- wp-content/themes/fancy-lab/header.php
+++ wp-content/themes/fancy-lab/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="profile" href="https://gmpg.org/xfn/11">
   </link>
-  <title>Fancy Lab</title>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
