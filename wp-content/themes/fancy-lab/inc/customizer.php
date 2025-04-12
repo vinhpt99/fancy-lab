f00b358abeda441c164602e64d2440ed988ba941
vinhpt99 <vinhpt72@gmail.com>
2025-04-12 18:19:06 +0700

    feat: added customize slider

--- wp-content/themes/fancy-lab/inc/customizer.php
+++ wp-content/themes/fancy-lab/inc/customizer.php
@@ -25,6 +25,138 @@ function fancy_lab_customizer($wp_customize)
       'type' => 'text',
     )
   );
-}
+
+  //Slider Section
+  $wp_customize->add_section('sec_slider', array(
+    'title' => 'Slider Settings',
+    'description' => 'Slider Section',
+  ));
+
+  //Field 1 - Slider Page Number 1
+  $wp_customize->add_setting('set_slider_page1', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'absint',
+  ));
+
+  $wp_customize->add_control('set_slider_page1', array(
+    'label' => 'Set slider page 1',
+    'description' => 'Select the page for slider 1',
+    'section' => 'sec_slider',
+    'type' => 'dropdown-pages',
+  ));
+
+  //Field 2 - Slider Button Text Number 
+  $wp_customize->add_setting('set_slider_button_text1', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'sanitize_text_field',
+  ));
+
+  $wp_customize->add_control('set_slider_button_text1', array(
+    'label' => 'Button Text for Page 1',
+    'description' => 'Button Text for Page 1',
+    'section' => 'sec_slider',
+    'type' => 'text',
+  ));
+
+  //Field 3 - Slider Button Link Number 1
+  $wp_customize->add_setting('set_slider_button_url1', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control('set_slider_button_url1', array(
+    'label' => 'Button URL for Page 1',
+    'description' => 'Button URL for Page 1',
+    'section' => 'sec_slider',
+    'type' => 'url',
+  ));
+
+  //Field 4 - Slider Page Number 2
+  $wp_customize->add_setting('set_slider_page2', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'absint',
+  ));
+
+  $wp_customize->add_control('set_slider_page2', array(
+    'label' => 'Set slider page 2',
+    'description' => 'Select the page for slider 2',
+    'section' => 'sec_slider',
+    'type' => 'dropdown-pages',
+  ));
+
+  //Field 5 
+  $wp_customize->add_setting('set_slider_button_text2', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'sanitize_text_field',
+  ));
+
+  $wp_customize->add_control('set_slider_button_text2', array(
+    'label' => 'Button Text for Page 1',
+    'description' => 'Button Text for Page 1',
+    'section' => 'sec_slider',
+    'type' => 'text',
+  ));
+
+  //Field 6
+  $wp_customize->add_setting('set_slider_button_url2', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control('set_slider_button_url2', array(
+    'label' => 'Button URL for Page 1',
+    'description' => 'Button URL for Page 1',
+    'section' => 'sec_slider',
+    'type' => 'url',
+  ));
+
+  //Field 7 - Slider Page Number 3
+  $wp_customize->add_setting('set_slider_page_3', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'absint',
+  ));
+
+  $wp_customize->add_control('set_slider_page_3', array(
+    'label' => 'Set slider page 1',
+    'description' => 'Select the page for slider 1',
+    'section' => 'sec_slider',
+    'type' => 'dropdown-pages',
+  ));
+
+  //Field 8 - Slider Button Text Number 
+  $wp_customize->add_setting('set_slider_button_text3', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'sanitize_text_field',
+  ));
+
+  $wp_customize->add_control('set_slider_button_text3', array(
+    'label' => 'Button Text for Page 1',
+    'description' => 'Button Text for Page 1',
+    'section' => 'sec_slider',
+    'type' => 'text',
+  ));
+
+  //Field 9 - Slider Button Link Number 3
+  $wp_customize->add_setting('set_slider_button_url3', array(
+    'type' => 'theme_mod',
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control('set_slider_button_url3', array(
+    'label' => 'Button URL for Page 1',
+    'description' => 'Button URL for Page 1',
+    'section' => 'sec_slider',
+    'type' => 'url',
+  ));
+};
 
 add_action('customize_register', 'fancy_lab_customizer');
