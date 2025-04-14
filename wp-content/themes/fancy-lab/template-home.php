497f71b9a5bcab05e1632461080b8a9d005e87b8
vinhpt99 <vinhpt72@gmail.com>
2025-04-14 22:55:20 +0700

    update

--- wp-content/themes/fancy-lab/template-home.php
+++ wp-content/themes/fancy-lab/template-home.php
@@ -54,55 +54,78 @@ get_header(); ?>
         </ul>
       </div>
     </section>
-    <section class="popular-product">
+    <?php if (class_exists('WooCommerce')): ?>
+      <section class="popular-product">
+        <?php
+        $popular_limit = get_theme_mod('set_popular_max_num', 4);
+        $popular_col = get_theme_mod('set_popular_max_col', 4);
+        $arrivals_limit = get_theme_mod('set_new_arrivals_max_num', 4);
+        $arrivals_col = get_theme_mod('set_new_arrivals_max_col', 4);
+
+        ?>
+        <div class="container">
+          <h2>Popular Product</h2>
+          <?php echo do_shortcode('[products limit="' . $popular_limit . '" columns="' . $popular_col . '" orderby="popularity"]'); ?>
+        </div>
+      </section>
+      <section class="new-arrivals">
+        <div class="container">
+          <h2>New Arrivals</h2>
+          <?php echo do_shortcode('[products limit="' . $arrivals_limit . '" columns="' . $arrivals_col . '" orderby="date"]'); ?>
+        </div>
+      </section>
       <?php
-      $popular_limit = get_theme_mod('set_popular_max_num', 4);
-      $popular_col = get_theme_mod('set_popular_max_col', 4);
-      $arrivals_limit = get_theme_mod('set_new_arrivals_max_num', 4);
-      $arrivals_col = get_theme_mod('set_new_arrivals_max_col', 4);
+      $showdeal = get_theme_mod('set_deal_show', 0);
+      $deal = get_theme_mod('set_deal');
+      $currency = get_woocommerce_currency_symbol();
+      $regular = floatval(get_post_meta($deal, '_regular_price', true));
+      $sale = floatval(get_post_meta($deal, '_sale_price', true));
 
+      if ($showdeal == 1 && (!empty($deal)) && $regular > 0 && $sale > 0):
+        $discount_percentage = absint(100 - (($sale / $regular) * 100));
+      else:
+        $discount_percentage = 0;
+      endif;
       ?>
-      <div class="container">
-        <h2>Popular Product</h2>
-        <?php echo do_shortcode('[products limit="' . $popular_limit . '" columns="' . $popular_col . '" orderby="popularity"]'); ?>
-      </div>
-    </section>
-    <section class="new-arrivals">
-      <div class="container">
-        <h2>New Arrivals</h2>
-        <?php echo do_shortcode('[products limit="' . $arrivals_limit . '" columns="' . $arrivals_col . '" orderby="date"]'); ?>
-      </div>
-    </section>
-    <?php
-    $showdeal = get_theme_mod('set_deal_show', 0);
-    $deal = get_theme_mod('set_deal');
-    $currency = get_woocommerce_currency_symbol();
-    $regular = floatval(get_post_meta($deal, '_regular_price', true));
-    $sale = floatval(get_post_meta($deal, '_sale_price', true));
-
-    if ($showdeal == 1 && (!empty($deal)) && $regular > 0 && $sale > 0):
-      $discount_percentage = absint(100 - (($sale / $regular) * 100));
-    else:
-      $discount_percentage = 0;
-    endif;
-    ?>
-    <section class="deal-of-the-week">
-      <div class="container">
-        <h2>Deal of the Week</h2>
-        <div class="flex items-center flex-wrap -mx-4">
-          <div class="deal-img col-md-6 col-12 text-center ml-auto">
-            <?php echo get_the_post_thumbnail($deal, 'large', array('class' => 'img-fluid')); ?>
-          </div>
-          <div class="deal-desc col-md-4 col-12 text-center mr-auto">
-            <span class="discount">
-              <?php echo $discount_percentage; ?>% OFF
-            </span>
+      <section class="deal-of-the-week">
+        <div class="container">
+          <h2>Deal of the Week</h2>
+          <div class="flex items-center flex-wrap -mx-4">
+            <div class="deal-img w-full md:w-1/2 text-center ml-auto">
+              <?php echo get_the_post_thumbnail($deal, 'large', array('class' => 'img-fluid')); ?>
+            </div>
+            <div class="deal-desc col-md-4 col-12 text-center mr-auto">
+              <?php if (!empty($sale)): ?>
+                <span class="discount">
+                  <?php echo $discount_percentage; ?>% OFF
+                </span>
+              <?php endif  ?>
+              <h3>
+                <a href="<?php echo get_the_permalink($deal) ?>"><?php echo get_the_title($deal) ?></a>
+              </h3>
+              <p><?php echo get_the_excerpt($deal); ?></p>
+              <div class="prices">
+                <span class="regular">
+                  <?php echo $currency;
+                  echo $regular;
+                  ?>
+                </span>
+                <?php if (!empty($sale)): ?>
+                  <span class="sale">
+                    <?php echo $currency;
+                    echo $sale;
+                    ?>
+                  </span>
+                <?php endif  ?>
+              </div>
+              <a href="<?php echo esc_url('?add-to-cart=' . $deal) ?>" class="add-to-cart">
+                Add to Cart
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-    <?php
-    ?>
+      </section>
+    <?php endif; ?>
     <section class="lab-blog">
       <div class="container">
         <div class="row">
